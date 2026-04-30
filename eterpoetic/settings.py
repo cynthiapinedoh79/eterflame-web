db9a654e410a64c59ad9387ffa4be0220e23c678
Cpinedo <cynthiapinedoh79@gmail.com>
2026-04-30 04:48:52 +0000

    fix: use port 465 SSL for SendGrid on Heroku

--- eterpoetic/settings.py
+++ eterpoetic/settings.py
@@ -299,8 +299,11 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "/"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+
 EMAIL_HOST_USER = 'apikey'
 EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_API_KEY')
 DEFAULT_FROM_EMAIL = 'studio@eterflame.com'
